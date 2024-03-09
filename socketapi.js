55204e066d71f1226626666cf866ef16a5bf1749
tyrant0407 <amankushwaha1331@gmail.com>
2024-03-09 15:29:29 +0530

    first commit

--- socketapi.js
+++ socketapi.js
@@ -7,10 +7,19 @@ const socketapi = {
 io.on( "connection", function( socket ) {
     console.log( "A user connected" );
 
-    socket.on("sony",message =>{
-        console.log(message)
+    socket.on("sony",messageObject =>{
+        console.log(messageObject)
         // io.emit('max',message)// jitne users connect hai sabko send kr deta hai
-        socket.broadcast.emit('max',message) // jisne send kiya usko chodkr sabko bhej dega
+        socket.broadcast.to(messageObject.roomName).emit('max',messageObject.message) 
+    })
+
+    // socket.on("sony",message =>{
+    //     console.log(message)
+    //     // io.emit('max',message)// jitne users connect hai sabko send kr deta hai
+    //     socket.broadcast.emit('max',message) // jisne send kiya usko chodkr sabko bhej dega
+    // })
+    socket.on('join-room',roomName =>{
+        socket.join('roomName')
     })
 });
 // end of socket.io logic
